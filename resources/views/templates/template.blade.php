99e6429ff24da4f49b86260daed9a1d65a6b1404
JanainaSilva <janainafsilva00@univem.edu.br>
2020-02-25 23:50:46 -0300

    Lesson 2 - creating new controller and base template

---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalale=no, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>CRUD com Laravel</title>
+        <link rel="stylesheet" href="{{ url('assets/bootstrap/css/bootstrap.min.css') }}">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
